1d205a46fee2744847bf892cf0e70a9f96bf3526
krisv <>
2022-02-25 17:56:52 +0100

    {/src/main/java/com/myspace/demo20220225/Person.java}

--- src/main/java/com/myspace/demo20220225/Person.java
+++ src/main/java/com/myspace/demo20220225/Person.java
@@ -6,12 +6,43 @@ package com.myspace.demo20220225;
 
 public class Person implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Person() {
-    }
+	private java.lang.String name;
+	private long age;
+	private java.lang.Boolean canBuy;
 
+	public Person() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public long getAge() {
+		return this.age;
+	}
+
+	public void setAge(long age) {
+		this.age = age;
+	}
+
+	public java.lang.Boolean getCanBuy() {
+		return this.canBuy;
+	}
+
+	public void setCanBuy(java.lang.Boolean canBuy) {
+		this.canBuy = canBuy;
+	}
+
+	public Person(java.lang.String name, long age, java.lang.Boolean canBuy) {
+		this.name = name;
+		this.age = age;
+		this.canBuy = canBuy;
+	}
 
 }
